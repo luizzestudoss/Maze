64004a7514bb968909783246e103fadac6a0b761
luizzestudoss <00001098581374sp@al.educacao.sp.gov.br>
2024-12-04 21:23:57 -0300

    acac

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,9 +1,15 @@
 let img;
+
 function preload() {
-    img = loadImage("assets/Sprites/Chest.png")
+    img = loadImage("assets/Sprites/Chest.png");
+}
+
+function setup() {
+    createCanvas(400, 400); // Adiciona um canvas para desenhar a imagem
 }
 
 function draw() {
-    image(img,0,0);
-    console.log(working)
+    background(220); // Adiciona um fundo
+    image(img, 0, 0);
+    console.log("working");
 }
